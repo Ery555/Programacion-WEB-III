7db6e4963d42b4ea31a21e17f543bdc298a98add
Ery555 <erick.semperteguif@gmail.com>
2026-03-26 17:52:27 -0400

    Solucion 1 Commit

--- Practica 1/Solucion_1.js
+++ Practica 1/Solucion_1.js
@@ -1,1 +1,14 @@
-console.log("HOLA MUNDO")
+function miFuncion(palabra) {
+    let resultado={}
+    for (let letra of palabra) {
+        if (resultado[letra]) {
+            resultado[letra]++;
+        }
+        else{
+            resultado[letra]=1;
+        }
+    }
+    return resultado;
+};
+let obj=miFuncion("casa");
+console.log(obj);
