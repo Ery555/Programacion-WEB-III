d974b35da9730a5e14348175994dca1f18047dec
Ery555 <erick.semperteguif@gmail.com>
2026-03-26 21:03:20 -0400

    Update Solucion_10.js

--- Practica 1/Solucion_10.js
+++ Practica 1/Solucion_10.js
@@ -1,9 +1,10 @@
 /**
-  EJERCICIO 10: ¿Cuándo usar Callbacks vs Promesas?
-  CALLBACKS: Convenientes para procesos simples, síncronos (como map/filter) 
-  o eventos únicos e inmediatos (como un click en el DOM).
-  PROMESAS: Necesarias para operaciones asíncronas complejas, encadenamiento 
-  de procesos y cuando se requiere un manejo de errores robusto (catch). 
+  EJERCICIO 10: ¿Cuándo usar Callbacks vs Promesas?  
+  CALLBACKS: Son convenientes para operaciones asíncronas simples, temporizadores
+   y manejo de eventos, también sirven en procesos sincronos.
+  PROMESAS: Necesarias para operaciones asíncronas complejas, con tareas
+  que tienen un tiempo indefinido en completarse, encadenamiento 
+  de procesos y cuando se requiere un manejo de errores robusto (catch).
  
  */
 console.log('Explicación en los comentarios del código')
